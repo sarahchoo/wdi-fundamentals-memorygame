9e3b9be8df7bbb471eba3ca62fc752ffef3929e1
Sarah Choo <sarahchoo@Sarahs-MacBook-Pro.local>
2019-05-21 00:19:11 +0800

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,9 +1,30 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+var cardThree = [];
+var cardFour = [];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped queen");
+console.log("User flipped king");
+
+if(cardsInPlay.length === 2) {
+	console.log("You have selected two cards.");
+}
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else { 
+	alert("Sorry, try again.");
+}
+
+
+
+
