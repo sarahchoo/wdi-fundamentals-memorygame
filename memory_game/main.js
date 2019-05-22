73e2b22f18f0ea664cb62fbbff669ceec6cd7ba8
Sarah Choo <sarahchoo@MacBook-Pro.sg.temasys.io>
2019-05-22 19:32:57 +0800

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,30 +1,28 @@
-console.log("Up and running!");
-
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-var cardThree = [];
-var cardFour = [];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped queen");
-console.log("User flipped king");
-
-if(cardsInPlay.length === 2) {
-	console.log("You have selected two cards.");
-}
-
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else { 
-	alert("Sorry, try again.");
-}
+		console.log("You found a match!"); 
+	} else {
+		console.log("Sorry, try again.");
+	}
+	}
+
+function flipCard(cardId) {
+
+cardsInPlay.push(cards[cardId]);	
+
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+	console.log("User flipped " + cards[cardId]);
+	return false;
+	};
 
 
+	flipCard(0);
+	flipCard(2);
 
 
